b982a294a145c78cae67bfa5ae534fccd3016af2
mohamed-bo <medprgrm@gmail.com>
2023-11-20 17:00:19 +0100

    inchalah it works

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[index], end='')
             j += 1
-        except Exception:
+        except Exception as ee:
             break
     print('')
     return j
