11da2e763d6b762d05d663ee48bb1dad752b1be5
mohamed-bo <medprgrm@gmail.com>
2023-12-05 23:36:42 +0100

    inchalah it works

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,29 @@
 #!/usr/bin/python3
-"""code add item"""
-
-import json
+"""
+code add item
+"""
 import sys
-import os.path
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-fn = "add_item.json"
-try:
-    jso = load_from_json_file(fn)
-except FileNotFoundError:
-    jso = []
-for i in range(1, len(sys.argv)):
-    jso.append(sys.argv[i])
-save_to_json_file(jso, fn)
+
+def add_item(args):
+    """
+    add function
+    """
+
+    fn = "add_item.json"
+
+    try:
+        lis = load_from_json_file(fn)
+    except FileNotFoundError:
+        lis = []
+    for i in range(1, len(args)):
+        lis.append(args[i])
+    save_to_json_file(lis, fn)
+
+
+if __name__ == "__main__":
+    add_item(sys.argv)
