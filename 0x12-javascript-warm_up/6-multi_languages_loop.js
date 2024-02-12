ff1fad8417bd2a1addae6827aecbab843f7a5b7e
mohamed-bo <medprgrm@gmail.com>
2024-02-12 17:33:09 +0100

    inchalah it works

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-for (let i of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+for (const i of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
   console.log(i);
 }
