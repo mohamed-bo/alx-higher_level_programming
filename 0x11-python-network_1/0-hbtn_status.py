1a8ff618be84f45732177e543d07e1c958503d15
mohamed-bo <medprgrm@gmail.com>
2024-03-01 18:30:43 +0100

    inchalah it works

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,8 +3,11 @@
 fetches
 """
 if __name__ == "__main__":
-    import urllib.request as request
-    from sys import argv
-    req = request.Request(argv[1])
-    with request.urlopen(req) as reqq:
-        print(reqq.headers.get('X-Request-Id'))
+    from urllib.request import Request, urlopen
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urlopen(url) as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
