8d7df32fa70e4bc90de2ce5fc41b463f9a84166e
mohamed-bo <medprgrm@gmail.com>
2024-03-26 17:55:43 +0100

    inchalah it works

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,24 +1,25 @@
 #!/usr/bin/node
+
 const request = require('request');
 
-request(process.argv[2], function (error, res, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    const cmpltedTask = {};
+request(process.argv[2], function (err, res, body) {
+  if (err) {
+    console.log(err);
+  } else if (res.statusCode === 200) {
+    const structu = {};
+    const todo = JSON.parse(body);
 
-    const bodyParsed = JSON.parse(body);
-    for (let i = 0; i < bodyParsed.length; ++i) {
-      const uid = bodyParsed[i].uid;
-      const finished = bodyParsed[i].completed;
-
-      if (finished && !cmpltedTask[uid]) {
-        cmpltedTask[uid] = 0;
-      }
-      if (finished) {
-        cmpltedTask[uid]++;
+    for (const i in todo) {
+      if (todo[i].completed) {
+        if (structu[todo[i].userId] === undefined) {
+          structu[todo[i].userId] = 1;
+        } else {
+          structu[todo[i].userId]++;
+        }
       }
     }
-    console.log(cmpltedTask);
+    console.log(structu);
+  } else {
+    console.log('Error code: ' + res.statusCode);
   }
 });
