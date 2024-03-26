1eee4b99cc97a8a8c800070f32813d2f6c84d218
mohamed-bo <medprgrm@gmail.com>
2024-03-26 17:49:43 +0100

    inchalah it works

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
-const api = 'http://swapi.co/api/films/' + process.argv[2];
+const api = 'http://swapi.dev/api/films/' + process.argv[2];
 
 request(api, function (err, res, body) {
   if (err) {
     console.log(err);
   } else if (res.statusCode === 200) {
-    body = JSON.parse(body);
-    console.log(body.title);
+    console.log(JSON.parse(body).title);
   } else {
     console.log('Erorr Code:' + res.statusCode);
   }
