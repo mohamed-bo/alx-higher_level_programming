c5d0096ff3781886084e987d1779a95531c9e394
mohamed-bo <medprgrm@gmail.com>
2024-03-26 17:56:50 +0100

    inchalah it works

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,9 +5,7 @@ const api = 'http://swapi.dev/api/films/' + process.argv[2];
 request(api, function (err, res, body) {
   if (err) {
     console.log(err);
-  } else if (res.statusCode === 200) {
-    console.log(JSON.parse(body).title);
   } else {
-    console.log('Erorr Code:' + res.statusCode);
+    console.log(JSON.parse(body).title);
   }
 });
