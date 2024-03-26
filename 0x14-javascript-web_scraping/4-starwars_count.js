3960bff062431624a18dc2b48cefeacb3eee42e4
mohamed-bo <medprgrm@gmail.com>
2024-03-26 17:52:17 +0100

    inchalah it works

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,7 +5,7 @@ request(process.argv[2], function (_error, _res, body) {
   const bodyParsed = JSON.parse(body).results;
   let occurenceNumber = 0;
   for (let i = 0; i < bodyParsed.length; ++i) {
-    const chrcts = bodyParsed[i].chrcts;
+    const chrcts = bodyParsed[i].characters;
 
     for (let j = 0; j < chrcts.length; ++j) {
       const character = chrcts[j];
